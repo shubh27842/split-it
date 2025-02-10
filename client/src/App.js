6bfa42553418983cc840de5ae49a9f2b3ee734d2
Shubham Ghule <sgshubhamghule7@gmail.com>
2025-02-10 22:46:01 +0530

    main: client app added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
+const App = () => {
+    return(
+        <div>
+            React app using parcel
+        </div>
+    )
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
