9826e56e1e2c70cf9c1a7709c90d0fa6277c981a
Shubham Ghule <sgshubhamghule7@gmail.com>
2025-02-13 10:02:34 +0530

    main: prettier added

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -2,17 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import pluginJest from "eslint-plugin-jest";
-
+import pluginPrettier from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: [ "dist/"]
+    ignores: ["dist/"],
   },
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
   },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
@@ -20,9 +20,20 @@ export default [
     languageOptions: { globals: globals.node }, // Allow Node.js globals like `module`
   },
   {
-    files: ["**/__tests__/**/*.[jt]s?(x)", "**/*.test.[jt]s?(x)","**/*.spec.[jt]s?(x)"],
+    files: [
+      "**/__tests__/**/*.[jt]s?(x)",
+      "**/*.test.[jt]s?(x)",
+      "**/*.spec.[jt]s?(x)",
+    ],
     languageOptions: { globals: globals.jest },
     plugins: { jest: pluginJest },
     rules: pluginJest.configs.recommended.rules,
-  }
+  },
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      ...pluginPrettier.configs.recommended.rules,
+      "prettier/prettier": ["error", { endOfLine: "lf" }],
+    },
+  },
 ];
